db469b0f1ed85170a753cde0e50d185cfbc2b99e
voidSatisfaction <lourie@naver.com>
2017-10-30 13:22:30 +0900

    chore: automatize deployment on client

--- src/env/config.js
+++ src/env/config.js
@@ -1,4 +1,5 @@
 export default {
-  host: 'http://localhost:3000',
-  // host: 'https://hidden-mesa-14684.herokuapp.com/'
+  // host: 'http://localhost:3000', // for dev
+  // host: 'http://localhost:9000', // for local server
+  host: 'https://ancient-ocean-58239.herokuapp.com/' // for deployement
 };
